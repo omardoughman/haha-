d7fa56c56a6881293e249286f48d39554c1dbe14
omardoughman <238817456+omardoughman@users.noreply.github.com>
2026-05-07 08:27:10 +0100

    Add project information script in Ruby

---
+++ project-info.rb
@@ -0,0 +1,8 @@
+# project_info.rb
+project_name = "haha!"
+author = "Omar"
+language = "Swift & Ruby"
+
+puts "Project: #{project_name}"
+puts "Developer: #{author}"
+puts "Main Language: #{language}"
